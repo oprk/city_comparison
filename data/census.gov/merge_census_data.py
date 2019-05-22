688445c6d2ee1208d99b9dfe0efaf3838ddf7015
John Theodore <johntheodore@github.com>
2019-05-21 22:05:04 -0700

    pass the build
    yapf passing

--- data/census.gov/merge_census_data.py
+++ data/census.gov/merge_census_data.py
@@ -1,13 +1,17 @@
-from pandas import read_csv, merge
+from pandas import read_csv
 
 census_city_populations_2010_filename = 'PEP_2017_PEPANNRSIP.US12A_with_ann.csv'
-pd_city_populations = read_csv(census_city_populations_2010_filename, encoding='ISO-8859-1')
+pd_city_populations = read_csv(census_city_populations_2010_filename,
+                               encoding='ISO-8859-1')
 
 census_city_areas_2010_filename = 'DEC_10_SF1_GCTPH1.US13PR_with_ann.csv'
 pd_city_areas = read_csv(census_city_areas_2010_filename, encoding='ISO-8859-1')
 
-pd_city_areas.rename(columns={'GCT_STUB.target-geo-id2': 'GC_RANK.target-geo-id2'}, inplace=True)
+pd_city_areas.rename(
+    columns={'GCT_STUB.target-geo-id2': 'GC_RANK.target-geo-id2'}, inplace=True)
 
-pd_combined_data = pd_city_populations.merge(pd_city_areas, on='GC_RANK.target-geo-id2', how='outer')
+pd_combined_data = pd_city_populations.merge(pd_city_areas,
+                                             on='GC_RANK.target-geo-id2',
+                                             how='outer')
 
 pd_combined_data.to_csv('census_combined_data.csv', index=False)
