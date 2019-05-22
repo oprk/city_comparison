ce9d00284daf83183c152c9043c4d2bce66f1e1a
John Theodore <johntheodore@github.com>
2019-05-22 09:18:55 -0700

    passing the build again

--- data/census.gov/merge_census_data.py
+++ data/census.gov/merge_census_data.py
@@ -1,21 +1,30 @@
+"""
+Merge 2 seperate csv files from census.gov into one combined csv.
+The two data sources are 2010 geography data by city, and,
+2017 census population data by city (including previous years to 2010)
+"""
+
 from pandas import read_csv
 
-census_city_populations_2010_filename = 'PEP_2017_PEPANNRSIP.US12A_with_ann.csv'
-pd_city_populations = read_csv(census_city_populations_2010_filename,
-                               encoding='ISO-8859-1',
-                               header=1)
-pd_city_populations = pd_city_populations.add_suffix(' census_2017')
 
-census_city_areas_2010_filename = 'DEC_10_SF1_GCTPH1.US13PR_with_ann.csv'
-pd_city_areas = read_csv(census_city_areas_2010_filename,
-                         encoding='ISO-8859-1',
-                         header=1)
-pd_city_areas = pd_city_areas.add_suffix(' census_2010')
+def merge_census_data():
+  """ Merge 2 census csv files """
+  census_city_populations_2010_filename = 'PEP_2017_PEPANNRSIP.US12A_with_ann.csv'
+  pd_city_populations = read_csv(census_city_populations_2010_filename,
+                                 encoding='ISO-8859-1',
+                                 header=1)
+  pd_city_populations = pd_city_populations.add_suffix(' census_2017')
+  census_city_areas_2010_filename = 'DEC_10_SF1_GCTPH1.US13PR_with_ann.csv'
+  pd_city_areas = read_csv(census_city_areas_2010_filename,
+                           encoding='ISO-8859-1',
+                           header=1)
+  pd_city_areas = pd_city_areas.add_suffix(' census_2010')
+  pd_combined_data = pd_city_populations.merge(
+      pd_city_areas,
+      left_on='Target Geo Id2 census_2017',
+      right_on='Target Geo Id2 census_2010',
+      how='inner')
+  pd_combined_data.to_csv('census_combined_data.csv', index=False)
 
-pd_combined_data = pd_city_populations.merge(
-    pd_city_areas,
-    left_on='Target Geo Id2 census_2017',
-    right_on='Target Geo Id2 census_2010',
-    how='inner')
 
-pd_combined_data.to_csv('census_combined_data.csv', index=False)
+merge_census_data()
--- data/experian/merge_experian_data.py
+++ data/experian/merge_experian_data.py
@@ -1,29 +1,37 @@
+""" Merge 3 seperate csv files from experian into one combined csv. """
+
 from pandas import read_csv
 
-cities_worst_credit_score_filename = '500_cities_worst_credit_score.csv'
-cities_worst_credit_score = read_csv(cities_worst_credit_score_filename,
-                               encoding='ISO-8859-1')
 
-cities_worst_credit_score = cities_worst_credit_score.drop('Rank', 1)
+def merge_experian_data():
+  cities_worst_credit_score_filename = '500_cities_worst_credit_score.csv'
+  cities_worst_credit_score = read_csv(cities_worst_credit_score_filename,
+                                       encoding='ISO-8859-1')
 
-cities_best_credit_score_filename = '500_cities_best_credit_score.csv'
-cities_best_credit_score = read_csv(cities_best_credit_score_filename,
-                               encoding='ISO-8859-1')
-cities_best_credit_score = cities_best_credit_score.drop('Rank', 1)
+  cities_worst_credit_score = cities_worst_credit_score.drop('Rank', 1)
 
-biggest_cities_best_credit_score_filename = '100_biggest_cities_best_credit_score.csv'
-biggest_cities_best_credit_score = read_csv(biggest_cities_best_credit_score_filename,
-                               encoding='ISO-8859-1')
-biggest_cities_best_credit_score = biggest_cities_best_credit_score.drop('Rank', 1)
+  cities_best_credit_score_filename = '500_cities_best_credit_score.csv'
+  cities_best_credit_score = read_csv(cities_best_credit_score_filename,
+                                      encoding='ISO-8859-1')
+  cities_best_credit_score = cities_best_credit_score.drop('Rank', 1)
 
-cities_credit_score_best_and_worst = cities_worst_credit_score.merge(
-    cities_best_credit_score,
-    on=['City', 'State', 'Credit Score'],
-    how='outer')
+  biggest_cities_best_credit_score_filename = '100_biggest_cities_best_credit_score.csv'
+  biggest_cities_best_credit_score = read_csv(
+      biggest_cities_best_credit_score_filename, encoding='ISO-8859-1')
+  biggest_cities_best_credit_score = biggest_cities_best_credit_score.drop(
+      'Rank', 1)
 
-all_cities_credit_scores = cities_credit_score_best_and_worst.merge(
-    biggest_cities_best_credit_score,
-    on=['City', 'State', 'Credit Score'],
-    how='outer')
+  cities_credit_score_best_and_worst = cities_worst_credit_score.merge(
+      cities_best_credit_score,
+      on=['City', 'State', 'Credit Score'],
+      how='outer')
 
-all_cities_credit_scores.to_csv('experian_combined_data.csv', index=False)
+  all_cities_credit_scores = cities_credit_score_best_and_worst.merge(
+      biggest_cities_best_credit_score,
+      on=['City', 'State', 'Credit Score'],
+      how='outer')
+
+  all_cities_credit_scores.to_csv('experian_combined_data.csv', index=False)
+
+
+merge_experian_data()
