b8b5691525aa43ec9065bd7eef69a2a0a937605e
John Theodore <johntheodore@github.com>
2019-05-21 23:24:40 -0700

    This is a combination of 2 commits.
    use read_csv to tell it which is the right header, and don't use next. add the combined census csv into the main csv, so we can get city area now, which allows us to get population density next.
    
    yapf

--- data/census.gov/merge_census_data.py
+++ data/census.gov/merge_census_data.py
@@ -2,16 +2,20 @@ from pandas import read_csv
 
 census_city_populations_2010_filename = 'PEP_2017_PEPANNRSIP.US12A_with_ann.csv'
 pd_city_populations = read_csv(census_city_populations_2010_filename,
-                               encoding='ISO-8859-1')
+                               encoding='ISO-8859-1',
+                               header=1)
+pd_city_populations = pd_city_populations.add_suffix(' census_2017')
 
 census_city_areas_2010_filename = 'DEC_10_SF1_GCTPH1.US13PR_with_ann.csv'
-pd_city_areas = read_csv(census_city_areas_2010_filename, encoding='ISO-8859-1')
+pd_city_areas = read_csv(census_city_areas_2010_filename,
+                         encoding='ISO-8859-1',
+                         header=1)
+pd_city_areas = pd_city_areas.add_suffix(' census_2010')
 
-pd_city_areas.rename(
-    columns={'GCT_STUB.target-geo-id2': 'GC_RANK.target-geo-id2'}, inplace=True)
-
-pd_combined_data = pd_city_populations.merge(pd_city_areas,
-                                             on='GC_RANK.target-geo-id2',
-                                             how='outer')
+pd_combined_data = pd_city_populations.merge(
+    pd_city_areas,
+    left_on='Target Geo Id2 census_2017',
+    right_on='Target Geo Id2 census_2010',
+    how='inner')
 
 pd_combined_data.to_csv('census_combined_data.csv', index=False)
