cc05665c561f540407ae5a5a5abe0c8b75795633
John Theodore <johntheodore@github.com>
2019-05-26 15:41:54 -0700

    add a run_tests_locally.py file so we can easily run tests locally

---
+++ run_tests_locally.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+A helper script to run test commands from the .travis.yml file locally
+in the dev environment.
+"""
+
+import subprocess
+import yaml
+
+with open('.travis.yml', 'r') as filehandler:
+  try:
+    TRAVIS_CONFIG = yaml.safe_load(filehandler)
+  except yaml.YAMLError as exc:
+    print(exc)
+
+TEST_COMMANDS = TRAVIS_CONFIG.get('script')
+
+EXIT_ERRORS = False
+for test_command in TEST_COMMANDS:
+  try:
+    subprocess.check_output(test_command, shell=True)
+  except subprocess.CalledProcessError as process_error:
+    print(test_command)
+    print(process_error.output)
+
+if EXIT_ERRORS:
+  exit(1)
